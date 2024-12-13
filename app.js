d31b57384aba0c6360ded2e177a8b2968e5fbb3d
NeneSan <nenesan6955nn@gmail.com>
2024-12-13 17:41:29 -0500

    Add api iframe YouTube and message width container.

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
 var url = document.URL
 var params = new URLSearchParams(window.location.search)
 var text = document.getElementById('getData')
-var iframeYT = document.getElementById('iframeYT')
-if (params!='null'){
-  var idYT = params.get('id')
-  var siYT = params.get('si')
-  text.innerHTML='Con parámetros | '+params+' | '+idYT+' | '+siYT
-  if(idYT && siYT){
-    text.innerHTML='Parámetros Completos | '+idYT+' | '+siYT
-    var urlYT = `https://www.youtube.com/embed/${idYT}?si=${siYT}?autoplay=1&mute=1&loop=1`
-    iframeYT.src=urlYT
-    text.innerHTML=urlYT
+// var iframeYT = document.getElementById('iframeYT')
 
+
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  if (params!='null'){
+    var idYT = params.get('id')
+    text.innerHTML='Con parámetros | '+params+' | '+idYT
+    if(idYT){
+      text.innerHTML='Parámetros Completos | '+idYT
+      var urlYT = `https://www.youtube.com/?id=${idYT}`
+      text.innerHTML=urlYT
+      player = new YT.Player('player', {
+          
+          videoId: idYT,  // Reemplaza 'VIDEO_ID' con el ID del video de YouTube
+          events: {
+              'onReady': onPlayerReady,
+              'onStateChange': onPlayerStateChange
+          }
+      });
+  
+    }
+  }else{
+    text.innerHTML='Sin Datos'+" | "+params
   }
-}else{
-  text.innerHTML='Sin Datos'+" | "+params
+    
 }
 
+function onPlayerReady(event) {
+    // Si quieres iniciar el video automáticamente
+    event.target.playVideo();
+}
 
-// text.innerHTML=params.get('id')
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+      console.log('El video está reproduciéndose');
+  } else if (event.data == YT.PlayerState.PAUSED) {
+      console.log('El video está pausado');
+  }
+}
+window.addEventListener('message', function(event) {
+  // if (event.origin === 'https://mi-dominio-con-cifrado.com') {
+      if (event.data === 'play') {
+          player.playVideo();  // Reproducir el video
+      } else if (event.data === 'pause') {
+          player.pauseVideo();  // Pausar el video
+      }
+  // }
+  console.log("[https] "+event.data)
+});
 
-// iframeYT.src='https://www.youtube.com/embed/Z63jUnZN1lw?si=KW09ZEsFAZhSU6o-'
+// setTimeout(()=>{player.playVideo();},3000)
 
-// https://youtu.be/Z63jUnZN1lw?si=KW09ZEsFAZhSU6o-
+// if (player) {
+//   player.pauseVideo();  // Pausa el video
+//   console.log('Pause');
+// }
 
-{/* <iframe  src="https://www.youtube.com/embed/Z63jUnZN1lw?si=KW09ZEsFAZhSU6o-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/m9SMT5ipbxk?si=6RVAtqR-9C8LfHvT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+// juoRKH4YWrA  DVRST, STM - Electric Dream
+// gyXy0m-4bvE  NU'EST(뉴이스트) _ FACE(페이스)
